bea45a0b9f2999fbdcc607de23bf991cd5013886
Jan Behrens <jb3@tzi.de>
2011-12-04 19:37:39 +0100

    Revert "branched the blog plugin as epos-diary"
    This reverts commit d391e6abf8cf4f76b2e908bb644baf26aee8492a.

--- diary.php
+++ diary.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Mahara: Electronic portfolio, weblog, resume builder and social networking
- * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ * Copyright (C) 2006-2010 Catalyst IT Ltd and others; see:
  *                         http://wiki.mahara.org/Contributors
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,54 +18,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
- * @subpackage artefact-blog
- * @author     Catalyst IT Ltd
+ * @subpackage artefact-epos
+ * @author     Jan Behrens
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2006-2009 Catalyst IT Ltd http://catalyst.net.nz
+ * @copyright  (C) 2011 Jan Behrens, jb3@informatik.uni-bremen.de
  *
  */
 
-define('INTERNAL', 1);
+define('INTERNAL', true);
 define('MENUITEM', 'diary');
 define('SECTION_PLUGINTYPE', 'artefact');
 define('SECTION_PLUGINNAME', 'epos');
 define('SECTION_PAGE', 'diary');
 
-require(dirname(dirname(dirname(__FILE__))) . '/init.php');
-safe_require('artefact', 'epos');
-
-define('TITLE', get_string('blogs','artefact.epos'));
-
-if ($delete = param_integer('delete', 0)) {
-    ArtefactTypeDiary::delete_form($delete);
-}
-
-$blogs = (object) array(
-    'offset' => param_integer('offset', 0),
-    'limit'  => param_integer('limit', 10),
-);
-
-list($blogs->count, $blogs->data) = ArtefactTypeDiary::get_blog_list($blogs->limit, $blogs->offset);
-
-ArtefactTypeDiary::build_blog_list_html($blogs);
-
-$smarty = smarty(array('paginator'));
-$smarty->assign_by_ref('blogs', $blogs);
-$smarty->assign('PAGEHEADING', TITLE);
-$smarty->assign('INLINEJAVASCRIPT', 'addLoadEvent(function() {' . $blogs->pagination_js . '});');
-$smarty->assign('MENUITEM', MENUITEM);
-$smarty->display('artefact:epos:diary.tpl');
-
-function delete_blog_submit(Pieform $form, $values) {
-    global $SESSION;
-    $blog = new ArtefactTypeDiary($values['delete']);
-    $blog->check_permission();
-    if ($blog->get('locked')) {
-        $SESSION->add_error_msg(get_string('submittedforassessment', 'view'));
-    }
-    else {
-        $blog->delete();
-        $SESSION->add_ok_msg(get_string('blogdeleted', 'artefact.epos'));
-    }
-    redirect('/artefact/epos/diary.php');
-}
+?>
