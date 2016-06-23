edd05a0d768a7c67830275e75cb98c917f23cb64
Jan Behrens <jb3@tzi.de>
2016-06-23 17:30:46 +0200

    Fixed deleting subjects

--- templates/deletesubject.json.php
+++ templates/deletesubject.json.php
@@ -36,7 +36,6 @@ $id = param_variable('id');
 try {
     // delete all links of the subject to descriptorsets and artefacts
     delete_records('artefact_epos_descriptorset_subject', 'subject', $id);
-    delete_records('artefact_epos_mysubject', 'subject', $id);
     delete_records('artefact_epos_subject', 'id', $id);
 
     //reply
