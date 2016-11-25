cad598bb4fc9e3006dd9b4991bd5adb2cbc9e8b7
Jan Behrens <jb3@tzi.de>
2016-11-25 19:37:41 +0100

    Added German and French translations

---
+++ blocktype/evaluation/lang/de.utf8/blocktype.evaluation.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('INTERNAL') || die();
+
+$string['defaulttitledescription'] = 'Wenn Sie nichts eintragen, wird der Name der Selbsteinschätzung verwendet';
+$string['description'] = 'Zeigen Sie Ihre Selbsteinschätzung für eine gewählte Sprache an';
+$string['selectevaluation'] = 'Wählen Sie die anzuzeigende Selbsteinschätzung aus';
+$string['title'] = 'Selbsteinschätzung';
